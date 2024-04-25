5f655cfc56fd7ed554495ae1596c75c2c3de8a45
Naveen <naveen.k.verma@crmnext.com>
2024-04-25 16:10:52 +0530

    Removed GCMUtil.kt class.

--- android/src/main/kotlin/com/nav/aes_gcm_plugin/AesGcmPlugin.kt
+++ android/src/main/kotlin/com/nav/aes_gcm_plugin/AesGcmPlugin.kt
@@ -1,12 +1,17 @@
 package com.nav.aes_gcm_plugin
 
 import androidx.annotation.NonNull
-
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import android.annotation.TargetApi
+import android.os.Build
+import java.util.Base64
+import javax.crypto.Cipher
+import javax.crypto.spec.GCMParameterSpec
+import javax.crypto.spec.SecretKeySpec
 
 /** AesGcmPlugin */
 class AesGcmPlugin: FlutterPlugin, MethodCallHandler {
@@ -32,7 +37,7 @@ class AesGcmPlugin: FlutterPlugin, MethodCallHandler {
       android.util.Log.d("AesGcmPlugin", "onMethodCall: "+plainText)
       android.util.Log.d("AesGcmPlugin", "onMethodCall: "+key)
       android.util.Log.d("AesGcmPlugin", "onMethodCall: "+iv)
-      val encryptedText = GCMUtil.encryptData(plainText,key,iv)
+      val encryptedText = encryptData(plainText,key,iv)
       result.success(encryptedText.toString())
 
     }
@@ -40,7 +45,7 @@ class AesGcmPlugin: FlutterPlugin, MethodCallHandler {
       var encryptedText : String? = call.argument("encryptedText");
       var key : String? = call.argument("key");
       var iv : String? = call.argument("iv");
-      val plainText = GCMUtil.decryptData(encryptedText,key,iv)
+      val plainText = decryptData(encryptedText,key,iv)
       result.success(plainText.toString())
     }
     else {
@@ -49,5 +54,42 @@ class AesGcmPlugin: FlutterPlugin, MethodCallHandler {
   }
   override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
     channel.setMethodCallHandler(null)
+  }
+
+  @TargetApi(Build.VERSION_CODES.O)
+  fun encryptData(plainText: String?, key: String?, iv: String?): String {
+//        checkEncryptionArguments(plainText, key, iv)
+
+    val pubKey = Base64.getDecoder().decode(key)
+    val nonce = iv?.toByteArray(Charsets.UTF_8)
+    val inputData = plainText?.toByteArray(Charsets.UTF_8)
+
+    val cipher = Cipher.getInstance("AES/GCM/NoPadding")
+    val keySpec = SecretKeySpec(pubKey, "AES")
+    val gcmParameterSpec = GCMParameterSpec(128, nonce)
+
+    cipher.init(Cipher.ENCRYPT_MODE, keySpec, gcmParameterSpec)
+    android.util.Log.d("GCMUtil", "encryptData: " + inputData.toString())
+    val encryptedData = cipher.doFinal(inputData)
+
+    return Base64.getEncoder().encodeToString(encryptedData)
+  }
+
+  @TargetApi(Build.VERSION_CODES.O)
+  fun decryptData(encryptedText: String?, key: String?, iv: String?): String {
+//        checkEncryptionArguments(encryptedText, key, iv)
+
+    val pubKey = Base64.getDecoder().decode(key)
+    val nonce = iv?.toByteArray(Charsets.UTF_8)
+    val encryptedData = Base64.getDecoder().decode(encryptedText)
+
+    val cipher = Cipher.getInstance("AES/GCM/NoPadding")
+    val keySpec = SecretKeySpec(pubKey, "AES")
+    val gcmParameterSpec = GCMParameterSpec(128, nonce)
+
+    cipher.init(Cipher.DECRYPT_MODE, keySpec, gcmParameterSpec)
+    val decryptedData = cipher.doFinal(encryptedData)
+
+    return String(decryptedData, Charsets.UTF_8)
   }
 }
